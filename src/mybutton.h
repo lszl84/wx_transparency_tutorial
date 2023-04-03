3e841a9a729109e4fc701797c52d32739e8f2cb6
Luke <luke@justdevtutorials.com>
2023-04-03 14:37:44 +0200

    solving the transparency problem in OnPaint for Windows

--- src/mybutton.h
+++ src/mybutton.h
@@ -9,8 +9,16 @@ class MyButton : public wxWindow
 {
 public:
     MyButton(wxWindow *parent, wxWindowID id, const wxPoint &pos, const wxSize &size, long style = 0, const wxString &name = wxPanelNameStr)
-        : wxWindow(parent, id, pos, size, style | wxFULL_REPAINT_ON_RESIZE, name)
+        : wxWindow()
     {
+        SetBackgroundStyle(wxBG_STYLE_TRANSPARENT);
+        wxWindow::Create(parent, id, pos, size, style, name);
+
+#if defined(__WXMSW__)
+        int extendedStyle = GetWindowLong(GetHWND(), GWL_EXSTYLE);
+        SetWindowLong(GetHWND(), GWL_EXSTYLE, extendedStyle | WS_EX_TRANSPARENT);
+#endif
+
         this->Bind(wxEVT_PAINT, &MyButton::OnPaint, this);
     }
 
